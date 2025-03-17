7c945ab6b453619dcbfffda1319cc2db08893052
Watchirawit Srisoonthornkasem <pluemwitzz@gmail.com>
2025-03-17 23:14:26 +0700

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} gradient-pastel min-h-screen`}>
+      <body className={inter.className}>
         <TopMenu />
         <main className="min-h-[calc(100vh-400px)]">
           {children}
